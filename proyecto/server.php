e9a278cc62a6e90dfde4c4cdacba4739ec699979
CieloGth <102554792+CieloGth@users.noreply.github.com>
2022-06-26 00:32:00 -0500

    COMMT T

---
+++ proyecto/server.php
@@ -0,0 +1,35 @@
+<?php
+    $nombre = $_POST["name"];
+    var_dump($nombre);
+    echo "<br>";
+
+    $email = $_POST["email"];
+    print_r($email);
+    echo "<br>";
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo("$email is a valid email address");
+      } else {
+        echo("$email is not a valid email address");
+      }
+    echo "<br>";
+    
+    $telefono = $_POST["tel"];
+    var_dump($telefono);
+    echo "<br>";
+    if (filter_var($telefono, FILTER_VALIDATE_INT) && (strlen($telefono)==10))  {
+        echo("$telefono is a valid email address");
+      } else {
+        echo("$telefono is not a valid email address");
+      }
+    echo "<br>";
+
+    $asunto = $_POST["subject"];
+    var_dump($asunto);
+    echo "<br>";
+
+    $mensaje = $_POST["message"];
+    var_dump($mensaje);
+    echo "<br>";
+
+
+?>
